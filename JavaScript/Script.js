1ad3a3905d3ceeea091b61602362985ce54386e2
LoganLogic <64856037+LoganLogic@users.noreply.github.com>
2020-09-02 12:56:05 -0400

    Folders, folders, folders

--- JavaScript/Script.js
+++ JavaScript/Script.js
@@ -3,44 +3,12 @@
 /*                                                                                                        */
 /*                                            Features                                                    */
 /*                                                                                                        */
-/*                          SlideShow: Displays Slideshow With Captions                                   */
 /*                        Screen Checker: Checks If Elements are on Screen                                */
 /*                                    Counter: Count Up Animation                                         */
 /*              Navbar Display: Slides In and out Navbar based on Screen Scrolling Up or Down             */
 /*                                                                                                        */
 /*--------------------------------------------------------------------------------------------------------*/
 
-/*-------------------------------------------SlideShow----------------------------------------------------*/
-var slideIndex = 1; // Current Slide
-
-showSlides(slideIndex); // Display Slide
-
-function plusSlides(n) {  // Go to Next Slide If Button Pressed
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {  // Go to Previous Slide If Button Pressed
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {  // Display Slides
-  var slides = document.getElementsByClassName("mySlides"); //Stylize Slides
-  var dots = document.getElementsByClassName("dot");  //Stylize Dots
-  if (n > slides.length) {slideIndex = 1} // Loop Back To First Slide
-  if (n < 1) {slideIndex = slides.length} // Loop Back To Last Slide
-  for (var i = 0; i < slides.length; i++) { // Set Every Slide to Not Be Visible
-      slides[i].style.display = "none";
-  }
-  for (var i = 0; i < dots.length; i++) { // Set Every Dot to Be Inactive
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block"; // Set Current Slide
-  dots[slideIndex-1].className += " active";  // Set Current Dot
-}
-
-function loadImage(n) { // Load Image
-    if(!LoadImage){showSlides(n)}
-}
 
 /*-----------------------------------Screen Checker and Counter--------------------------------------------*/
 
