5bbc1885328a0c619cd839205f3b065110807e3f
LoganLogic <64856037+LoganLogic@users.noreply.github.com>
2020-09-02 22:10:46 -0400

    Better Website

--- JavaScript/Script.js
+++ JavaScript/Script.js
@@ -71,3 +71,14 @@ var currentScrollPos = window.pageYOffset;
   }
   prevScrollpos = currentScrollPos;
 }
+/*------------------------------------------TESTING--------------------------------------------------------*/
+// When the user scrolls down 20px from the top of the document, slide down the navbar
+window.onscroll = function() {scrollFunction()};
+    document.getElementById("navbar").style.opacity = "0";
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("navbar").style.opacity = "0.9";
+  } else {
+    document.getElementById("navbar").style.opacity = "0";
+  }
+}
