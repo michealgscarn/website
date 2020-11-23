11dcbb9af96468b968079c772bf82f152b596ad9
Michael Scott <56855793+michealgscarn@users.noreply.github.com>
2020-11-22 19:16:34 -0500

    Implimented new font

--- JavaScript/Contact.js
+++ JavaScript/Contact.js
@@ -4,6 +4,6 @@ const form = document.forms['google-sheet']
 form.addEventListener('submit', e => {
   e.preventDefault()
   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-    .then(response => alert("Thanks for Contacting us..! We Will Contact You Soon..."))
+    .then(response => alert("Thanks for Contacting us! We Will Send a Response Shortly"))
     .catch(error => console.error('Error!', error.message))
 })
