89fcfcaf0016e7551a5ebb6b10b27ffcb80dd695
tdaug6 <57757881+tdaug6@users.noreply.github.com>
2020-11-25 18:22:18 -0500

    replaced

---
+++ JavaScript/Navbar.js
@@ -0,0 +1,22 @@
+/*--------------------------------------------SCRIPT.js---------------------------------------------------*/
+/*                                  Created By: Logan Daugherty                                           */
+/*                                                                                                        */
+/*                                            Features                                                    */
+/*                                                                                                        */
+/*              Navbar Display: Slides In and out Navbar based on Screen Scrolling Up or Down             */
+/*                                                                                                        */
+/*--------------------------------------------------------------------------------------------------------*/
+
+
+/*----------------------------------------Navbar Display---------------------------------------------------*/
+
+// When the user scrolls down 20px from the top of the document, slide down the navbar
+window.onscroll = function() {scrollFunction()};
+    document.getElementById("navbar").style.opacity = "0";
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("navbar").style.opacity = "1";
+  } else {
+    document.getElementById("navbar").style.opacity = "0";
+  }
+}
