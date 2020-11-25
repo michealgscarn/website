56b63fcaffb57c12b502ad5c309ce6663b81fece
tdaug6 <57757881+tdaug6@users.noreply.github.com>
2020-11-25 18:20:33 -0500

    deleted

--- JavaScript/Contact.js
+++
@@ -1,9 +0,0 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbzpExCoqZ08rWot17QWXZtnk0VovQr9QQCroOfwqk7NHrfdvKI/exec'
-const form = document.forms['google-sheet']
-
-form.addEventListener('submit', e => {
-  e.preventDefault()
-  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-    .then(response => alert("Thanks for Contacting us! We Will Send a Response Shortly"))
-    .catch(error => console.error('Error!', error.message))
-})
