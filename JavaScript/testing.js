7cb51168503519c060067fd43e0a2c06c8dab1db
Michael Scott <56855793+michealgscarn@users.noreply.github.com>
2020-11-29 11:01:05 -0500

    pictures

--- JavaScript/testing.js
+++ JavaScript/testing.js
@@ -10,8 +10,8 @@
   function isElementInViewport(el) {
     var rect = el.getBoundingClientRect();
     return (
-      rect.top >= 0 &&
-      rect.left >= 0 &&
+      rect.top >= -100 &&
+      rect.left >= -100 &&
       rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
       rect.right <= (window.innerWidth || document.documentElement.clientWidth)
     );
